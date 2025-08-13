9aa98286c6c68f370ca09d1991577e134d8bef16
Deepika Srivastava <dpka9509@gmain.com>
2025-08-13 15:15:56 +0530

    Add assignment.java

---
+++ assignment.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class assignment {
+     public static void main(String[] args) {
+//ques1
+        // System.out.println("Welcome to Bridgelabz!");
+
+        Scanner sc = new Scanner(System.in);
+//ques2
+        // System.out.print("Enter a: ");
+        // int a = sc.nextInt();
+
+        // System.out.print("Enter b: ");
+        // int b = sc.nextInt();
+
+        // System.out.println("Sum: " + (a + b));
+
+//ques3
+        // System.out.print("Enter temperature in Celsius: ");
+        // double celsius = sc.nextDouble();
+        // double fahrenheit = (celsius * 9 / 5) + 32;
+        // System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+
+//ques4
+        // System.out.print("Enter the radius of the circle: ");
+        // double radius = sc.nextDouble();
+        // double area = Math.PI * radius * radius;
+        // System.out.println("Area of the circle: " + area);
+
+//ques5
+        // System.out.print("Enter radius: ");
+        // double radius = sc.nextDouble();
+        // System.out.print("Enter height: ");
+        // double height = sc.nextDouble();
+        // double volume = Math.PI * radius * radius * height;
+        // System.out.println("Volume of the cylinder: " + volume);   
+        
+// Self-problems
+ //ques1
+        // System.out.print("Enter Principal: ");
+        // double principal = sc.nextDouble();
+        // System.out.print("Enter Rate: ");
+        // double rate = sc.nextDouble();
+        // System.out.print("Enter Time: ");
+        // double time = sc.nextDouble();
+        // double interest = (principal * rate * time) / 100;
+        // System.out.println("Simple Interest: " + interest);
+
+//ques2
+        // System.out.print("Enter length: ");
+        // double length = sc.nextDouble();
+        // System.out.print("Enter width: ");
+        // double width = sc.nextDouble();
+        // double perimeter = 2 * (length + width);
+        // System.out.println("Perimeter of the rectangle: " + perimeter);
+        
+//ques3
+        // System.out.print("Enter base: ");
+        // double base = sc.nextDouble();
+        // System.out.print("Enter exponent: ");
+        // double exponent = sc.nextDouble();
+        // double result = Math.pow(base, exponent);
+        // System.out.println("Result: " + result);
+
+//ques4
+        // System.out.print("Enter first number: ");
+        // double num1 = sc.nextDouble();
+        // System.out.print("Enter second number: ");
+        // double num2 = sc.nextDouble();
+        // System.out.print("Enter third number: ");
+        // double num3 = sc.nextDouble();
+        // double average = (num1 + num2 + num3) / 3;
+        // System.out.println("Average: " + average);
+
+//ques5
+        // System.out.print("Enter distance in kilometers: ");
+        // double km = sc.nextDouble();
+        // double miles = km * 0.621371;
+        // System.out.println("Distance in miles: " + miles);        
+    }
+}
+
