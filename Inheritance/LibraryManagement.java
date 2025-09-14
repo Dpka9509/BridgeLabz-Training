a3997e1a15448d10dd31f992240b7791c9e93dd9
Deepika Srivastava <dpka9509@gmain.com>
2025-09-14 20:04:42 +0530

    Added Library Management System with Book and Author classes

---
+++ Inheritance/LibraryManagement.java
@@ -0,0 +1,49 @@
+// Superclass: Book
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    void displayInfo() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
+
+// Subclass: Author (extending Book)
+class Author extends Book {
+    String name;
+    String bio;
+
+    Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author Name: " + name);
+        System.out.println("Bio: " + bio);
+        System.out.println("---------------------------");
+    }
+}
+
+// Main class
+public class LibraryManagement {
+    public static void main(String[] args) {
+        Author a1 = new Author(
+            "Effective Java", 
+            2018, 
+            "Joshua Bloch", 
+            "Joshua Bloch is a software engineer and author, known for his work at Sun Microsystems and Google."
+        );
+
+        a1.displayInfo();
+    }
+}
