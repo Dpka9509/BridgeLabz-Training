286a60a92e66e63e3e7e12a8fd7b4a4ad360d082
Deepika Srivastava <dpka9509@gmain.com>
2025-09-26 17:31:57 +0530

    Added MostFrequentChar program

---
+++ Extra String/MostFrequentChar.java
@@ -0,0 +1,22 @@
+public class MostFrequentChar {
+    public static void main(String[] args) {
+        String str = "success";
+        int[] freq = new int[256];
+
+        for (char c : str.toCharArray()) {
+            freq[c]++;
+        }
+
+        char maxChar = ' ';
+        int max = -1;
+
+        for (char c : str.toCharArray()) {
+            if (freq[c] > max) {
+                max = freq[c];
+                maxChar = c;
+            }
+        }
+
+        System.out.println("Most Frequent Character: '" + maxChar + "'");
+    }
+}
